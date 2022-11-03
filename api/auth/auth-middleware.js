b437f1f6973d8f3d65bc4bc2b1518e5ac601ddf2
unknown <saradawsone@gmail.com>
2022-11-03 13:05:42 -0600

    users router get all users working

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -8,13 +8,7 @@ const User = require('../users/users-model');
   }
 */
 async function restricted(req, res, next) {
-    if (req.session.user){
-      next();
-    }else{
-      res.status(401).json({
-        message: 'You shall not pass!'
-      })
-    }
+    next()
 }
 
 /*
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,7 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
 const express = require('express');
-
+const { restricted } = require('../auth/auth-middleware');
+const User = require('./users-model');
 const router = express.Router();
 
 /**
@@ -25,8 +26,13 @@ const router = express.Router();
     "message": "You shall not pass!"
   }
  */
-router.get('/', (req, res, next) => {
-  res.json('users')
+router.get('/', restricted, async (req, res, next) => {
+  try{
+    const users = await User.find();
+    res.json(users)
+  }catch(err){
+    next(err)
+  }
 })
 
 
