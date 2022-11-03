31893b0c0287ffbc963f7429c53559b9f5c3414f
unknown <saradawsone@gmail.com>
2022-11-03 12:53:11 -0600

    auth router setup

--- api/server.js
+++ api/server.js
@@ -3,7 +3,8 @@ const session = require('express-session');
 const Store = require('connect-session-knex')(session);
 const helmet = require("helmet");
 const cors = require("cors");
-
+const usersRouter = require('./users/users-router');
+const authRouter = require('./auth/auth-router');
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -22,6 +23,8 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
 server.use(session({
   name: 'chocolatechip',
   secret: 'keep it secret',
